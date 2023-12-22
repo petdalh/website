64c7a0e834e4ca97ec0618acb512c07f45a12ee2
Petter Dalhaug <124355644+petdalh@users.noreply.github.com>
2023-12-22 18:08:51 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function App() {
+  return (
+    <div className="p-4 max-w-sm mx-auto">
+    </div>
+  );
+}
+
+export default App;
+
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust this path according to your project structure
+    // Include any other paths that may contain Tailwind CSS classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
